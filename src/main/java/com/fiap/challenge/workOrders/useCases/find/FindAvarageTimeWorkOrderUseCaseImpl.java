3ea045e1db2ebc35f122cf595b94837f45200aac
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-08-12 02:03:27 -0300

    feat: adiciona caso de uso listar todos os registros

--- src/main/java/com/fiap/challenge/workOrders/useCases/find/FindAvarageTimeWorkOrderUseCaseImpl.java
+++ src/main/java/com/fiap/challenge/workOrders/useCases/find/FindAvarageTimeWorkOrderUseCaseImpl.java
@@ -1,11 +1,14 @@
 package com.fiap.challenge.workOrders.useCases.find;
 
+import com.fiap.challenge.shared.model.ResponseApi;
 import com.fiap.challenge.workOrders.entity.WorkOrderAvarageTime;
 import com.fiap.challenge.workOrders.repository.AvarageTimeWorkOrderRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -19,5 +22,14 @@ public class FindAvarageTimeWorkOrderUseCaseImpl implements FindAvarageTimeWorkO
         WorkOrderAvarageTime workOrderAvarageTime = avarageTimeWorkOrderRepository.findByWorkOrderId(id).orElse(null);
         if (Objects.isNull(workOrderAvarageTime)) throw new EntityNotFoundException("Entity not found with id: " + id);
         return workOrderAvarageTime;
+    }
+
+    @Override
+    public ResponseApi<List<WorkOrderAvarageTime>> executeList() {
+        ResponseApi<List<WorkOrderAvarageTime>> responseApi = new ResponseApi<>();
+        List<WorkOrderAvarageTime> workOrderAvarageTimes = avarageTimeWorkOrderRepository.findAll();
+        if (workOrderAvarageTimes.isEmpty()) return responseApi.of(HttpStatus.NOT_FOUND,"No average time work orders found.");
+
+        return responseApi.of(HttpStatus.OK, "Average time work orders retrieved successfully.", workOrderAvarageTimes);
     }
 }
