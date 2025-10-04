1e8ec5626568d0ad2873a5ad29c252c255642fe8
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-10-04 01:25:09 -0300

    refact: altera tipo de retorno

--- application/src/main/java/com/fiap/application/usecaseimpl/FindUserByIdUseCaseImpl.java
+++ application/src/main/java/com/fiap/application/usecaseimpl/FindUserByIdUseCaseImpl.java
@@ -1,22 +1,23 @@
 package com.fiap.application.usecaseimpl;
 
-import com.fiap.application.gateway.FindUserByIdGateway;
+import com.fiap.application.gateway.UserGateway;
 import com.fiap.core.domain.User;
-import com.fiap.core.exception.EmailException;
+import com.fiap.core.exception.NotFoundException;
 import com.fiap.usecase.FindUserByIdUseCase;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class FindUserByIdUseCaseImpl implements FindUserByIdUseCase {
 
-    final FindUserByIdGateway findUserByIdGateway;
+    final UserGateway userGateway;
 
-    public FindUserByIdUseCaseImpl(FindUserByIdGateway findUserByIdGateway) {
-        this.findUserByIdGateway = findUserByIdGateway;
+    public FindUserByIdUseCaseImpl(UserGateway userGateway) {
+        this.userGateway = userGateway;
     }
 
     @Override
-    public User execute(UUID userId) {
-        return findUserByIdGateway.findById(userId);
+    public Optional<User> execute(UUID userId) throws NotFoundException {
+        return userGateway.findById(userId);
     }
 }
