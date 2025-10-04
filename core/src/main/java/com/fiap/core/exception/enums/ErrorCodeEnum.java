b44bb4bbbb57d11ce0457f7d0498867d428e6712
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-10-04 01:24:29 -0300

    feat: altera tags de erros relacionados ao usuário

--- core/src/main/java/com/fiap/core/exception/enums/ErrorCodeEnum.java
+++ core/src/main/java/com/fiap/core/exception/enums/ErrorCodeEnum.java
@@ -6,16 +6,17 @@ public enum ErrorCodeEnum {
     CAD0002("Documento já cadastrado!", "CAD-0002"),
     CAD0003("Email já cadastrado!", "CAD-0003"),
     CAD0004("Erro na criação do cliente", "CAD-0004"),
-    CAD0005("A senha não pode ser nula", "CAD-0005"),
-    CAD0006("A senha deve ter pelo menos 8 caracteres", "CAD-0006"),
-    CAD0007("A senha deve conter pelo menos uma letra maiúscula", "CAD-0007"),
-    CAD0008("A senha deve conter pelo menos uma letra minúscula", "CAD-0008"),
-    CAD0009("A senha deve conter pelo menos um número", "CAD-0009"),
-    CAD0010("A senha deve conter pelo menos um caractere especial (!@#$%^&*()-+)", "CAD-0010"),
-    CAD0011("Erro ao validar senha", "CAD-0011")
 
-    CUST0001("Cliente não encontrado", "CUST-0001")
-    ;
+    USE0001("A senha não pode ser nula", "USE-0001"),
+    USE0002("A senha deve ter pelo menos 8 caracteres", "USE-00022"),
+    USE0003("A senha deve conter pelo menos uma letra maiúscula", "USE-0003"),
+    USE0004("A senha deve conter pelo menos uma letra minúscula", "USE-0004"),
+    USE0005("A senha deve conter pelo menos um número", "USE-0005"),
+    USE0006("A senha deve conter pelo menos um caractere especial (!@#$%^&*()-+)", "USE-0006"),
+    USE0007("Usuário não encontrado", "USE-0007"),
+    USE0008("Erro na criação do usuário", "USE-0008"),
+
+    CUST0001("Cliente não encontrado", "CUST-0001");
 
     private String message;
     private String code;
