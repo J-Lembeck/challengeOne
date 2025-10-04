d40aeffe263c33e7814381857e426195d6553f3a
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-10-04 01:21:32 -0300

    refactor: Altera tipo de retorno

--- application/src/main/java/com/fiap/application/usecaseimpl/CreateUserUseCaseImpl.java
+++ application/src/main/java/com/fiap/application/usecaseimpl/CreateUserUseCaseImpl.java
@@ -1,22 +1,30 @@
 package com.fiap.application.usecaseimpl;
 
-import com.fiap.application.gateway.CreateUserGateway;
+import com.fiap.application.gateway.UserGateway;
 import com.fiap.core.domain.User;
+import com.fiap.core.exception.InternalServerErrorException;
+import com.fiap.core.exception.enums.ErrorCodeEnum;
 import com.fiap.usecase.CreateUserUseCase;
+
+import java.util.Objects;
 
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
-    final CreateUserGateway createUserGateway;
+    final UserGateway userGateway;
 
-    public CreateUserUseCaseImpl(CreateUserGateway createUserGateway) {
-        this.createUserGateway = createUserGateway;
+    public CreateUserUseCaseImpl(UserGateway userGateway) {
+        this.userGateway = userGateway;
 
     }
 
     @Override
-    public void execute(User user) {
-        if(!createUserGateway.create(user)) {
-            throw new RuntimeException("Failed to create user");
+    public User execute(User user) throws InternalServerErrorException {
+        User userCreated = userGateway.create(user);
+        if(Objects.isNull(userCreated)) {
+          throw new InternalServerErrorException(
+              ErrorCodeEnum.USE0008.getMessage(), ErrorCodeEnum.USE0008.getCode());
         }
+
+        return userCreated;
     }
 }
