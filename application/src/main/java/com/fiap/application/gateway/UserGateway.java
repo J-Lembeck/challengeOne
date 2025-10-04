6c0c7d884472c72b2c1a73d032d78e5bc98ad5e4
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-10-04 01:35:23 -0300

    refact: altera nome do arquivo, adiciona cruds

--- application/src/main/java/com/fiap/application/gateway/UserGateway.java
+++ application/src/main/java/com/fiap/application/gateway/UserGateway.java
@@ -1,7 +1,14 @@
 package com.fiap.application.gateway;
 
 import com.fiap.core.domain.User;
+import com.fiap.core.exception.NotFoundException;
 
-public interface CreateUserGateway {
-    Boolean create(User user);
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserGateway {
+    User create(User user);
+    User update(User user);
+    Optional<User> findById(UUID userId) throws NotFoundException;
+    void delete(User user);
 }
